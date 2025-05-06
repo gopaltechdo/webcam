d2b13695db470fadcfce35df7e7b549f0381c98c
gopal kumar <gopalsingh.g60@gmail.com>
2025-05-06 10:52:20 +0530

    dfa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import WebcamStream from './webcam';
-// const App = () => {
-//   const videoURL = " https://10.214.115.50:5000/video_feed"; // replace with server IP or domain
-//   // const videoURL = "https://flask-nr63.onrender.com"; // replace with server IP or domain
-
-
-//   return (
-//     <div className="p-4 text-center">
-//       <h1 className="text-2xl font-bold mb-4">Live Webcam Feed</h1>
-//       <img src={videoURL} alt="Webcam Stream" className="mx-auto border-4 border-orange-300" />
-//     </div>
-//   );
-// };
-
-// export default App;
 const App = () => {
+  const videoURL = " http://192.168.1.18:5000/video_feed"; // replace with server IP or domain
+  // const videoURL = "https://flask-nr63.onrender.com"; // replace with server IP or domain
+
+
   return (
-    <div className="App">
-      <WebcamStream />
+    <div className="p-4 text-center">
+      <h1 className="text-2xl font-bold mb-4">Live Webcam Feed</h1>
+      <img src={videoURL} alt="Webcam Stream" className="mx-auto border-4 border-orange-300" />
     </div>
   );
-}
+};
+
 export default App;
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <WebcamStream />
+//     </div>
+//   );
+// }
+// export default App;
