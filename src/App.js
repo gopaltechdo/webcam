389f2ea2a8069c071210a842d3b5cc88468a464f
gopal kumar <gopalsingh.g60@gmail.com>
2025-05-06 09:48:26 +0530

    '.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const App = () => {
-  const videoURL = "http://192.168.1.18:5000/video_feed"; // replace with server IP or domain
+  const videoURL = " http://10.214.115.50:5000/video_feed"; // replace with server IP or domain
+  // const videoURL = "https://flask-nr63.onrender.com"; // replace with server IP or domain
+
 
   return (
     <div className="p-4 text-center">
